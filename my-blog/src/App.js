1749c19833947c6510722764baca2f85481ea926
Mark_Leo <ratrateroo@gmail.com>
2022-05-29 07:36:42 +0800

    removed link

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -23,15 +23,7 @@ function App() {
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+         
           </VStack>
         </Grid>
       </Box>
