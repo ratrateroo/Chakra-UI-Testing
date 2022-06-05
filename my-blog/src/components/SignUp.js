0f1aac41f64ecbbf910a6487baf4d9dc127974f5
Mark_Leo <ratrateroo@gmail.com>
2022-06-05 22:16:16 +0800

    added sign up component

---
+++ my-blog/src/components/SignUp.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Box,
+  Button,
+} from '@chakra-ui/react';
+
+const SignUp = () => {
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <FormControl>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input id="username" type="text" />
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input id="password" type="password" />
+        <FormHelperText>We'll never share your information.</FormHelperText>
+      </FormControl>
+      <Button colorScheme="blue">Button</Button>
+    </Box>
+  );
+};
+
+export default SignUp;
