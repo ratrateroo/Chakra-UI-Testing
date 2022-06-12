6ba21f64bde46ae23d3bd5f4de303a690536bdee
Mark_Leo <ratrateroo@gmail.com>
2022-06-12 22:55:19 +0800

    added nav links for log in and sign up

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -26,6 +26,12 @@ const App = () => {
         <Link to="/about" style={{ padding: 5 }}>
           About
         </Link>
+        <Link to="/login" style={{ padding: 5 }}>
+          Log In
+        </Link>
+        <Link to="/signup" style={{ padding: 5 }}>
+          Sign Up
+        </Link>
       </nav>
       <Box textAlign="center" fontSize="xl">
         <Navigation />
