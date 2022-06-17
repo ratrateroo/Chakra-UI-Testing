59918ef5d62321e8967086f71bc4a809627c88a6
Mark_Leo <ratrateroo@gmail.com>
2022-06-17 19:13:31 +0800

    added login and signup pages to the app

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,12 +4,16 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Routes, Link, Outlet, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/home';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
 
 const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <nav style={{ margin: 10 }}>
         <Link to="/login" style={{ padding: 5 }}>
