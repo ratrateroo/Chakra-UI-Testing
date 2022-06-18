b666f636f3755a25c7a5edf08edfa00852027ae5
Mark_Leo <ratrateroo@gmail.com>
2022-06-18 22:14:18 +0800

    fixed navigation location

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -15,21 +15,14 @@ const App = () => {
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-      <nav style={{ margin: 10 }}>
-        <Link to="/login" style={{ padding: 5 }}>
-          Log In
-        </Link>
-        <Link to="/signup" style={{ padding: 5 }}>
-          Sign Up
-        </Link>
-      </nav>
+
       <Box textAlign="center" fontSize="xl">
         <Navigation />
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
         </Grid>
+        <Outlet />
       </Box>
-      <Outlet />
     </ChakraProvider>
   );
 };
--- my-blog/src/components/Navigation.js
+++ my-blog/src/components/Navigation.js
@@ -6,8 +6,12 @@ const Navigation = () => {
     <Box textAlign="center" fontSize="xl">
       <Flex>
         <HStack>
-          <Link>Log In</Link>
-          <Link>Sign Up</Link>
+          <Link to="/login" style={{ padding: 5 }}>
+            Log In
+          </Link>
+          <Link to="/signup" style={{ padding: 5 }}>
+            Sign Up
+          </Link>
         </HStack>
       </Flex>
     </Box>
